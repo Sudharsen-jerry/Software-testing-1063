3506244f9e22933cadd931bbb7fcc0edd9bafcfc
Sudharsen-jerry <128677099+Sudharsen-jerry@users.noreply.github.com>
2023-10-22 18:26:03 +0530

    Delete NumberOperations.java

--- NumberOperations.java
+++
@@ -1,9 +0,0 @@
-public class NumberOperations {
-    public static double calculateSquare(double number) {
-        return number * number;
-    }
-
-    public static double calculateCube(double number) {
-        return number * number * number;
-    }
-}
