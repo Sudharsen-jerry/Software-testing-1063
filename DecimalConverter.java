0f7491e2b2a8c09ef4c14910d79d3dab14493a53
Sudharsen-jerry <128677099+Sudharsen-jerry@users.noreply.github.com>
2023-10-23 12:13:05 +0530

    Delete DecimalConverter.java

--- DecimalConverter.java
+++
@@ -1,9 +0,0 @@
-public class DecimalConverter {
-    public static String decimalToBinary(int decimalNum) {
-        return Integer.toBinaryString(decimalNum);
-    }
-
-    public static String decimalToOctal(int decimalNum) {
-        return Integer.toOctalString(decimalNum);
-    }
-}
