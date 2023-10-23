60223cc4f90d129b54b493a192dc2905a06fe923
Sudharsen-jerry <128677099+Sudharsen-jerry@users.noreply.github.com>
2023-10-23 13:01:36 +0530

    Update simpleinterest_eclip.java

--- simpleinterest_eclip.java
+++ simpleinterest_eclip.java
@@ -1,27 +1,35 @@
-package cohong;
+package DOHONG;
+
 import java.util.Scanner;
 
-public class Interest {
+public class exp4 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter principal amount (P):");
         float P = sc.nextFloat();
 
-        System.out.println("Enter rate of interest (R):");
-        float R = sc.nextFloat();
-
         System.out.println("Enter time period (T):");
         float T = sc.nextFloat();
 
-        float SI = (P * T * R) / 100;
-        System.out.println("Simple interest = " + SI);
+        System.out.println("Are you a senior citizen? (yes/no):");
+        String isSenior = sc.next();
 
-        // Manual check
-        if (3600 == SI) {
-            System.out.println("Assertion passed!");
-        } else {
-            System.out.println("Assertion failed!");
-        }
+        float rateOfInterest = getRateOfInterest(isSenior);
+        float SI = calculateSimpleInterest(P, T, rateOfInterest);
+
+        System.out.println("Simple interest = " + SI);
+    }
+
+    // Function to calculate simple interest
+    public static float calculateSimpleInterest(float principal, float time, float rateOfInterest) {
+        return (principal * time * rateOfInterest) / 100;
+    }
+
+    // Function to determine the rate of interest based on customer type
+    public static float getRateOfInterest(String customerType) {
+        // Senior citizen gets 12%, others get 10%
+        return customerType.equalsIgnoreCase("yes") ? 12 : 10;
     }
 }
