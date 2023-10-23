76e8dadf7d75cf6a22599fae16942b4253f2f684
Sudharsen-jerry <128677099+Sudharsen-jerry@users.noreply.github.com>
2023-10-23 12:16:20 +0530

    Delete StringComparator.java

--- StringComparator.java
+++
@@ -1,6 +0,0 @@
-public class StringComparator {
-    public boolean compareStrings(String str1, String str2) {
-        // Comparing two strings using equals method
-        return str1.equals(str2);
-    }
-}
