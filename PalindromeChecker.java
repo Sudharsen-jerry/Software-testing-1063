c1c4aed10a7527a65959fef2bc9a0660a89d2c6f
Sudharsen-jerry <128677099+Sudharsen-jerry@users.noreply.github.com>
2023-10-23 12:15:31 +0530

    Delete PalindromeChecker.java

--- PalindromeChecker.java
+++
@@ -1,7 +0,0 @@
-public class PalindromeChecker {
-    public static boolean isPalindrome(int number) {
-        String original = String.valueOf(number);
-        String reversed = new StringBuilder(original).reverse().toString();
-        return original.equals(reversed);
-    }
-}
